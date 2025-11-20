de9370ca4ec9fe4fe218d04de68b2c9178dec9f0
dcntrl <dcntrlx@gmail.com>
2025-11-20 20:46:41 +0300

    StorageLock.sol optimized for better comparison to TransientLock.sol

--- contracts/StorageLock.sol
+++ contracts/StorageLock.sol
@@ -2,13 +2,19 @@
 pragma solidity ^0.8.30;
 
 contract StorageLock {
-    bool private locked;
-
     modifier nonReentrant() {
-        require(!locked);
-        locked = true;
+        assembly {
+            if sload(1) {  // Using slot 1 as reentrancy flag. Only appropriate for comparison to TransientStorage. Not recommended in real projects
+                revert(0, 0)
+            }
+            sstore(1, 1)
+        }
+        
         _;
-        locked = false;
+
+        assembly {
+            sstore(1, 0)
+        }
     }
 
     function doSomething() external nonReentrant {
